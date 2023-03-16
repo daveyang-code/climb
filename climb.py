191e5c6432a02d29c3205452bacae4e6989c0d76
David Yang <david.gfx@gmail.com>
2023-03-15 20:24:44 -0400

    Bug fix

--- climb.py
+++ climb.py
@@ -6,84 +6,85 @@ mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
 def drawLines(landmarks):
-    cv2.line(
-        image,
-        (
-            int(landmarks.landmark[15].x * vid_width),
-            int(landmarks.landmark[15].y * vid_height),
-        ),
-        (
-            int(landmarks.landmark[27].x * vid_width),
-            int(landmarks.landmark[27].y * vid_height),
-        ),
-        (255, 0, 0),
-        2
-    )
-    cv2.line(
-        image,
-        (
-            int(landmarks.landmark[15].x * vid_width),
-            int(landmarks.landmark[15].y * vid_height),
-        ),
-        (
-            int(landmarks.landmark[28].x * vid_width),
-            int(landmarks.landmark[28].y * vid_height),
-        ),
-        (255, 0, 0),
-        2
-    )
-    cv2.line(
-        image,
-        (
-            int(landmarks.landmark[16].x * vid_width),
-            int(landmarks.landmark[16].y * vid_height),
-        ),
-        (
-            int(landmarks.landmark[27].x * vid_width),
-            int(landmarks.landmark[27].y * vid_height),
-        ),
-        (0, 0, 255),
-        2
-    )
-    cv2.line(
-        image,
-        (
-            int(landmarks.landmark[16].x * vid_width),
-            int(landmarks.landmark[16].y * vid_height),
-        ),
-        (
-            int(landmarks.landmark[28].x * vid_width),
-            int(landmarks.landmark[28].y * vid_height),
-        ),
-        (0, 0, 255),
-        2
-    )
-    cv2.line(
-        image,
-        (
-            int(landmarks.landmark[15].x * vid_width),
-            int(landmarks.landmark[15].y * vid_height),
-        ),
-        (
-            int(landmarks.landmark[16].x * vid_width),
-            int(landmarks.landmark[16].y * vid_height),
-        ),
-        (255, 0, 255),
-        2
-    )
-    cv2.line(
-        image,
-        (
-            int(landmarks.landmark[27].x * vid_width),
-            int(landmarks.landmark[27].y * vid_height),
-        ),
-        (
-            int(landmarks.landmark[28].x * vid_width),
-            int(landmarks.landmark[28].y * vid_height),
-        ),
-        (255, 0, 255),
-        2
-    )
+    if landmarks:
+        cv2.line(
+            image,
+            (
+                int(landmarks.landmark[15].x * vid_width),
+                int(landmarks.landmark[15].y * vid_height),
+            ),
+            (
+                int(landmarks.landmark[27].x * vid_width),
+                int(landmarks.landmark[27].y * vid_height),
+            ),
+            (255, 0, 0),
+            2
+        )
+        cv2.line(
+            image,
+            (
+                int(landmarks.landmark[15].x * vid_width),
+                int(landmarks.landmark[15].y * vid_height),
+            ),
+            (
+                int(landmarks.landmark[28].x * vid_width),
+                int(landmarks.landmark[28].y * vid_height),
+            ),
+            (255, 0, 0),
+            2
+        )
+        cv2.line(
+            image,
+            (
+                int(landmarks.landmark[16].x * vid_width),
+                int(landmarks.landmark[16].y * vid_height),
+            ),
+            (
+                int(landmarks.landmark[27].x * vid_width),
+                int(landmarks.landmark[27].y * vid_height),
+            ),
+            (0, 0, 255),
+            2
+        )
+        cv2.line(
+            image,
+            (
+                int(landmarks.landmark[16].x * vid_width),
+                int(landmarks.landmark[16].y * vid_height),
+            ),
+            (
+                int(landmarks.landmark[28].x * vid_width),
+                int(landmarks.landmark[28].y * vid_height),
+            ),
+            (0, 0, 255),
+            2
+        )
+        cv2.line(
+            image,
+            (
+                int(landmarks.landmark[15].x * vid_width),
+                int(landmarks.landmark[15].y * vid_height),
+            ),
+            (
+                int(landmarks.landmark[16].x * vid_width),
+                int(landmarks.landmark[16].y * vid_height),
+            ),
+            (255, 0, 255),
+            2
+        )
+        cv2.line(
+            image,
+            (
+                int(landmarks.landmark[27].x * vid_width),
+                int(landmarks.landmark[27].y * vid_height),
+            ),
+            (
+                int(landmarks.landmark[28].x * vid_width),
+                int(landmarks.landmark[28].y * vid_height),
+            ),
+            (255, 0, 255),
+            2
+        )
 
 cap = cv2.VideoCapture("boulder.mp4")
 vid_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
@@ -111,7 +112,7 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             results.pose_landmarks,
             landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
         )
-        cv2.imshow("", cv2.flip(image, 1))
+        cv2.imshow("", image)
         if cv2.waitKey(5) & 0xFF == 27:
             break
         
